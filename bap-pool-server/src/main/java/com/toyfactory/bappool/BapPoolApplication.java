ad0494b6a5db6d5d211abaec04fddc8f6cc4a97c
SuyeonChoi <tndus970120@gmail.com>
2021-07-11 23:37:34 +0900

    init:서버 초기 설정

---
+++ bap-pool-server/src/main/java/com/toyfactory/bappool/BapPoolApplication.java
@@ -0,0 +1,13 @@
+package com.toyfactory.bappool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BapPoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BapPoolApplication.class, args);
+	}
+
+}
---
+++ bap-pool-server/src/test/java/com/toyfactory/bappool/BappoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyfactory.bappool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BappoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
