a7d7b710f07e30a71cb5b639e82b26189c92c077
SuyeonChoi <tndus970120@gmail.com>
2021-07-26 02:07:15 +0900

    feat: 카카오 음식점 영업 정보 크롤링

--- bap-pool-server/src/main/java/com/toyfactory/bappool/dto/EateryResponse.java
+++ bap-pool-server/src/main/java/com/toyfactory/bappool/dto/EateryResponse.java
@@ -1,4 +1,32 @@
 package com.toyfactory.bappool.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
 public class EateryResponse {
+    private String id;
+
+    private String place_name;
+
+    private String thumbnail;
+
+    private String distance;
+
+    private int click;
+
+    private String category_name;
+
+    private String place_url;
+
+    public EateryResponse(KakaoPlaceDocumentResponse kakaoResponse, String thumbnail, int click) {
+        this.id = kakaoResponse.getId();
+        this.place_name = kakaoResponse.getPlace_name();
+        this.distance = kakaoResponse.getDistance();
+        this.category_name = kakaoResponse.getCategory_name();
+        this.place_url = kakaoResponse.getPlace_url();
+        this.thumbnail = thumbnail;
+        this.click = click;
+    }
 }
--- bap-pool-server/src/main/java/com/toyfactory/bappool/service/EateryService.java
+++ bap-pool-server/src/main/java/com/toyfactory/bappool/service/EateryService.java
@@ -5,6 +5,12 @@ import com.toyfactory.bappool.domain.EateryRepository;
 import com.toyfactory.bappool.dto.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.http.*;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
@@ -13,8 +19,11 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.persistence.EntityNotFoundException;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -24,19 +33,63 @@ public class EateryService {
 
     private final EateryRepository eateryRepository;
 
+    private WebDriver driver;
+    private WebElement element;
+    private String url;
+
     public List<EateryResponse> findFive(double lng, double lat) {
+        List<EateryResponse> openPlace = new LinkedList<>();
+
         // Kakao API 호출
         List<KakaoPlaceDocumentResponse> places = callKakaoApi(lng, lat);
+        execChromeDriver();
         places.forEach(place -> {
-            if (!eateryRepository.existsById(Long.parseLong(place.getId()))) {
-                // 썸네일 크롤링 추가해야함
+            long id = Long.parseLong(place.getId());
+            if (!eateryRepository.existsById(id)) {
                 String thumbnailUrl = callKakaoImageApi(place.getPlace_name());
                 EateryCreateRequest request = new EateryCreateRequest(Long.parseLong(place.getId()), 0, thumbnailUrl);
                 create(request);
             }
+
+            try {
+                if (isOpen(place.getPlace_url())) {
+                    Eatery eatery = eateryRepository.findById(id)
+                            .orElseThrow(() -> new EntityNotFoundException("해당되는 음식점 정보가 없습니다."));
+
+                    openPlace.add(new EateryResponse(place, eatery.getThumbnail(), eatery.getClick()));
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         });
 
-        return null;
+        driver.close();
+
+        return openPlace.size() >= 5 ? openPlace.subList(0, 5) : openPlace;
+    }
+
+    private void execChromeDriver() {
+        String WEB_DRIVER_ID = "webdriver.chrome.driver";
+        String WEB_DRIVER_PATH = "/Users/suyeonchoi/Desktop/bap-pool/bap-pool-server/src/chromedriver";
+        System.setProperty(WEB_DRIVER_ID, WEB_DRIVER_PATH);
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        options.addArguments("--disable-popup-blocking");
+
+        driver = new ChromeDriver(options);
+    }
+
+    private boolean isOpen(String url) throws InterruptedException {
+        driver.get(url);
+        Thread.sleep(2000); // 3. 페이지 로딩 대기 시간
+
+        try {
+            driver.findElement(By.className("open"));
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
     }
 
     private String callKakaoImageApi(String placeName) {
@@ -80,6 +133,7 @@ public class EateryService {
         // Request REST API
         String baseUrl = "https://dapi.kakao.com/v2/local/search/category.json";
 
+        // 반경 추가해야함.
         UriComponents uriComponents = UriComponentsBuilder.fromHttpUrl(baseUrl)
                 .queryParam("category_group_code", "FD6")
                 .queryParam("sort", "accuracy")
