fa506cbea45b8e96775c5d75c7f3ce1af5732760
SuyeonChoi <tndus970120@gmail.com>
2021-08-22 19:30:07 +0900

    feat: CORS 설정 추가

--- bap-pool-server/src/main/java/com/toyfactory/bappool/controller/EateryController.java
+++ bap-pool-server/src/main/java/com/toyfactory/bappool/controller/EateryController.java
@@ -7,6 +7,7 @@ import com.toyfactory.bappool.service.EateryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @Slf4j
+@CrossOrigin("*")
 @RequiredArgsConstructor
 @RequestMapping("/api/eateries")
 @RestController
